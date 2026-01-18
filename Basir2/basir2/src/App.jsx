d5f460530a2cd05006da7ceb6a4a8f5107ff3f28
shekh basir <shekhbasir9821273349@gmail.com>
2026-01-18 09:41:29 +0530

    simple

--- Basir2/basir2/src/App.jsx
+++ Basir2/basir2/src/App.jsx
@@ -2,39 +2,11 @@ import React from "react";
 import Details from "./components/Details";
 
 function App() {
-  //from here i am gong to ending the data
-  const kam = [
-    {
-      name: "sheikh basir",
-      add: "bara Nepal",
-      marks: 23,
-    },
-    {
-      name: "komal",
-      add: "routahat",
-      marks: 43,
-    },
-    {
-      name: "pinkey yadav",
-      add: "parwanipur Bara",
-      marks: 40,
-    },
-    {
-      name: "Niva Yadav",
-      add: "Nehal",
-      marks: 50,
-    },
-  ];
-  console.log(kam);
   return (
-    <>
-      {kam.map((kam1) => (
-        <Details alldata={kam1} />
-      ))}
-    </>
+    <div>
+      <Details />
+    </div>
   );
 }
 
 export default App;
-
-//now i am going to use the props concept
